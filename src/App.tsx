0e3d0a71849a7524839fa9a8e83553f5cf005a44
alexchegash <aliskander84@gmail.com>
2020-05-08 18:17:58 +0300

    fixed hands generate logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,57 @@ import {playCard} from './types'
 import {suits as suitsConst, cards as cardsConst} from './constants'
 
 export const App: React.FC = () => {
-  const [hand1, setHand1] = useState<playCard[]>(hand())
-  const [hand2, setHand2] = useState<playCard[]>(hand())
+  const [hand1, setHand1] = useState(hands().hand1)
+  const [hand2, setHand2] = useState(hands().hand2)
 
-  function random(limit: number): number {
+  function replay() {
+    const h = hands()
+    setHand1(h.hand1)
+    setHand2(h.hand2)
+  }
+
+  function hands(): {hand1: playCard[], hand2: playCard[]} {
+    const cards = deal()
+    const hand1 = cards.slice(0,5)
+    const hand2 = cards.slice(5,10)
+    return {hand1, hand2}
+  }
+  
+  function deal(): playCard[] {
+    let cards = []
+    while (cards.length < 10) {
+      const card = playCard()
+      const isDuplicated = cards.filter(c => match(c, card)).length !== 0
+      if (!isDuplicated) {
+        cards.push(card)
+      }
+    }
+    return cards
+  }
+
+  function match(playCard1: playCard, playCard2: playCard) {
+    const isSameSuit = playCard1.suit === playCard2.suit
+    const isSameCard = playCard1.card === playCard2.card
+    return isSameSuit && isSameCard
+  }
+
+  function playCard(): playCard {
+    const suit = suitsConst[random(suitsConst.length)]
+    const card = cardsConst[random(cardsConst.length)]
+    return {suit, card}
+  }
+
+  function random(limitExcluded: number): number {
     let rand = Math.floor(Math.random() * 100)
-    while (rand > limit) {
+    while (rand >= limitExcluded) {
       rand = Math.floor(Math.random() * 100)
     }
     return rand
   }
 
-  function hand(): playCard[] {
-    let cards: playCard[] = []
-    while (cards.length < 5) {
-      cards.push({
-        suit: suitsConst[random(3)],
-        card: cardsConst[random(12)]
-      })
-    }
-    return cards
-  }
-
-  function hands(): void {
-    setHand1(hand())
-    setHand2(hand())
-  }
-
   return (<>
     <Hand playCards={hand1} player={1} winning={true}/>
     <Hand playCards={hand2} player={2}/>
-    <Button click={hands}/>
+    <Button click={replay}/>
   </>)
 }
